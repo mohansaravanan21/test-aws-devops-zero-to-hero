406a3bcaf307844ba2269c7585fbe65ca9c165b5
Mohan Saravanan <48146661+mohansaravanan21@users.noreply.github.com>
2025-06-25 02:23:58 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi AWS"
-# conid = `docker ps | awk -F " " '{print $1}'`
+echo "Hi AWS Mohan"
 
+
+# conid = `docker ps | awk -F " " '{print $1}'`
 # docker rm -f $conid

5cb2cf8449e60b1d350759c8846efa3ce26d140b
Mohan Saravanan <48146661+mohansaravanan21@users.noreply.github.com>
2025-06-25 01:29:02 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,7 +3,6 @@ set -e
 
 # Pull the Docker image from Docker Hub
 docker pull mohansaravanan212/simple-python-flask-app
-
 # Run the Docker image as a container
 docker run -d -p 5000:5000 mohansaravanan212/simple-python-flask-app
 
